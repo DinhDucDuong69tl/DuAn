bed2e371229080190ec83fd79a17619e27218910
mintam04 <mjntem2004@gmail.com>
2023-11-13 11:03:21 +0700

    ui-trangchu

---
+++ phanmem/home.php
@@ -0,0 +1,83 @@
+ <!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="dlabnav">
+            <div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+					<li class="dropdown header-profile">
+						<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+							<img src="images/profile/pic1.jpg" width="20" alt="">
+							<div class="header-info ms-3">
+								<span class="font-w600 ">Hi,<b>William</b></span>
+								<small class="text-end font-w400">william@gmail.com</small>
+							</div>
+						</a>
+						<div class="dropdown-menu dropdown-menu-end">
+							<a href="app-profile.html" class="dropdown-item ai-icon">
+								<svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18" height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+								<span class="ms-2">Profile </span>
+							</a>
+							<a href="page-error-404.html" class="dropdown-item ai-icon">
+								<svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+								<span class="ms-2">Logout </span>
+							</a>
+						</div>
+					</li>
+                    <li><a class="" href="index.php" >
+							<i class="flaticon-025-dashboard"></i>
+							<span class="nav-text">Trang Chủ</span>
+						</a>
+                        <!-- <ul aria-expanded="false">
+							<li><a href="index.html">Dashboard Light</a></li>
+							<li><a href="index-2.html">Dashboard Dark</a></li>
+							<li><a href="my-wallet.html">My Wallet</a></li>
+							<li><a href="page-invoices.html">Invoices</a></li>
+							<li><a href="cards-center.html">Cards Center</a></li>
+							<li><a href="page-transaction.html">Transaction</a></li>
+							<li><a href="transaction-details.html">Transaction Details</a></li>	
+						</ul> -->
+					</li>	
+					<li><a class="" href="#" >
+							<i class="flaticon-013-checkmark"></i>
+							<span class="nav-text">Nạp tiền</span>
+						</a>
+                    </li>		
+					<li><a class="" href="#" >
+							<i class="flaticon-043-menu"></i>
+							<span class="nav-text">Bill</span>
+						</a>
+                    </li>		
+                </ul>
+				
+			</div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
+		
+		
+<!--**********************************
+     Home
+***********************************-->	
+
+    <div class="content-body">
+                <div class="container-fluid">
+                    <div class="col-xl-6">
+                        <div class="card">
+                            <div class="card-header">
+                                <img class="card-img-top img-fluid" src="images/card/1.png" alt="Card image cap">                              
+                            </div>
+                            <div class="card-body">
+                            <b><h5 class="card-title">Card title</h5></b>
+                            <p class="card-text">This is a wider card with supporting text and below as a natural lead-in to the additional content. This content is a little <br> bit longer. Some quick example text to build the bulk</p>
+                            </div>
+                            <div class="card-footer d-sm-flex justify-content-between align-items-center">
+                                <div class="card-footer-link mb-4 mb-sm-0">
+                                    <p class="card-text text-dark d-inline">Last updated 3 mins ago</p>
+                                </div>
+                                <a href="javascript:void(0);" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
---
+++ phanmem/index.php
@@ -0,0 +1,10 @@
+<?php
+    require "../model/danhmuc.php";
+    require "../model/phanmem.php";
+    require "header.php";
+
+    // $listphanmem = loadall_phanmem();
+    // $listdanhmuc = loadall_danhmuc();
+    require "home.php";
+    require "footer.php";
+?>
