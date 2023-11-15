82840f1799c90db963b2275249e4bceb85599d76
mintam04 <mjntem2004@gmail.com>
2023-11-15 16:41:35 +0700

    ui-trangchu

---
+++ model/bill.php
@@ -0,0 +1,23 @@
+<?php
+    function insert_bill($id_phanmem, $id_user, $date){
+        $sql = "INSERT INTO `bill` (`id_phanmem`, `id_user`, `date`) VALUES ('{$id_phanmem}', '{$id_user}', '{$date}');";
+        pdo_execute($sql);
+    }
+
+    function loadall_bill(){
+        $sql = "SELECT * FROM `bill`";
+        $listbill = pdo_query($sql);
+        return $listbill;
+    }
+
+    function load_bill_user($id_user){
+        $sql = "SELECT * FROM `bill`
+        JOIN `user` ON bill.id_user = user.id_user
+        JOIN `phanmem` ON bill.id_phanmem = phanmem.id_phanmem;";
+        $listbill = pdo_query($sql);
+        return $listbill;
+    }
+
+
+
+?>
