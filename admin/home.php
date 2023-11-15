bc80bb84e0e4f62c29102025ffa9cd4f081d0e68
mintam04 <mjntem2004@gmail.com>
2023-11-15 10:06:46 +0700

    ui-trangchu

---
+++ admin/home.php
@@ -0,0 +1,24 @@
+
+		
+<!--**********************************
+     Home
+***********************************-->	
+    <div class="content-body">
+                <div class="container-fluid">        
+					<div class="row">
+						<div class="col-xl-6">
+								<div class="card">
+									<img class="card-img-top img-fluid" src="" alt="Card image cap">
+									<div class="card-header">
+										<h5 class="card-title">Card title</h5>
+									</div>
+									<div class="card-body">
+										<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+										<p class="card-text text-dark">Last updated 3 mins ago</p>
+									</div>
+								</div>
+						</div>
+					</div>           
+                    
+                </div>
+    </div>
