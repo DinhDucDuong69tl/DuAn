316e46d4a531fbdd762b5589f5f40d4068acf441
mintam04 <mjntem2004@gmail.com>
2023-11-16 17:05:18 +0700

    delete all

--- admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "../model/pdo.php";
-    require "../model/danhmuc.php";
-    require "../model/phanmem.php";
-    require "../model/bill.php";
-    require "header.php";
-    require "home.php";
-    require "footer.php";
-?>
