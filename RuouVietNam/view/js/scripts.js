de1ab4f76b05d06f8b4974a1c6d853b1f9369bd5
mintam04 <mjntem2004@gmail.com>
2023-11-16 15:40:11 +0700

    xoa

--- RuouVietNam/view/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.parallax').parallax();
-    $('.myreviews').carousel({
-        numVisible: 7,
-        shift: 55,
-        padding: 55,
-    });
-    $('.slider').slider({ full_width: true });
-});
